23b04b1278c292ee2d0f71981ac18b64f708ddfd
htaylor91 <htaylor91@gmail.com>
2023-02-17 15:34:40 +0100

    feat(Collapse): ✨ add Collapse component with details and summary elements

---
+++ src/components/Collapse.jsx
@@ -0,0 +1,31 @@
+import '../styles/Collapse.css';
+
+export default function Collapse({details, summary, page}) {
+    const type = summary;
+    const currentPage = page;
+    const textClass = currentPage === "logement" ? "details-text logement-details" : "details-text values-details";
+    const summaryClass = currentPage === "logement" ? "logement-summary" : "values-summary";
+    function detailsText() {
+        if(type !== "Équipements"){
+            return <p className={textClass}>{details}</p>
+        } else {
+            return (
+                <ul className={textClass}>
+                    {details.map((detail, index) => {
+                        return <li key={index}>{detail}</li>
+                    })}
+                </ul>
+            )
+        }
+    }
+
+    return (
+        <details className='details collapse' open>
+            <summary className='inversed summary'>
+                <span className={summaryClass}>{summary}</span>
+                <span className='summary-icon'></span>
+            </summary>
+            {detailsText()}
+        </details>
+    );
+}
