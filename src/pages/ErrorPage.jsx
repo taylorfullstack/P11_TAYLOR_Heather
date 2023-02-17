036db4c1aec03b97bbcb4db6d1bf402ccb10c489
htaylor91 <htaylor91@gmail.com>
2023-02-17 14:59:54 +0100

    feat(ErrorPage): ✨ add ErrorPage for status 404 errors

---
+++ src/pages/ErrorPage.jsx
@@ -0,0 +1,19 @@
+import { useRouteError, Link } from "react-router-dom";
+import "../styles/ErrorPage.css";
+import Header from "../components/Header.jsx";
+
+export default function ErrorPage() {
+    const error = useRouteError();
+    console.error(error);
+    
+    return (
+        <>
+        <Header />
+        <section id="error-page" className="error-page">
+            <h1>404</h1>
+            <p>Oups! La page que vous demandez n'existe pas.</p>
+            <Link to="/">Retourner sur la page d'accueil</Link>
+        </section>
+        </>
+    );
+}
