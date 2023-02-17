3dd378594cfebbf989017add07f701428e14573c
htaylor91 <htaylor91@gmail.com>
2023-02-17 15:05:54 +0100

    feat(LogementPage): ✨ add LogementPage to wrap unique logement articles

---
+++ src/pages/LogementPage.jsx
@@ -0,0 +1,10 @@
+import logements from '../data/logements.json';
+import Logement from '../components/Logement.jsx';
+
+export default function LogementPage() {
+    const path = window.location.pathname;
+    const id = path.split("/").at(-1);
+    const logement = logements.find(logement => logement.id === id);
+    return (<Logement key={logement.id} {...logement} />)
+}
+
