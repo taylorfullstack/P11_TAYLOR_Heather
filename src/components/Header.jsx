0caa08cfec2c7ea298a31947d25150b370f80c54
htaylor91 <htaylor91@gmail.com>
2023-02-17 14:56:22 +0100

    feat(Header): ✨ add Header component that wraps nav and navlinks

---
+++ src/components/Header.jsx
@@ -0,0 +1,33 @@
+import { NavLink } from "react-router-dom";
+import "../styles/Header.css";
+import logo from "../assets/logo.svg";
+
+export default function Header() {
+    return (
+        <header className="header-primary">
+            <img alt="Kasa" src={logo}></img>
+            <nav>
+                <ul>
+                    <li>
+                        <NavLink 
+                            style={({ isActive }) => {
+                                return {
+                                    textDecoration: isActive ? "underline" : "none",
+                                };
+                            }}
+                        to="/">Accueil</NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            style={({ isActive }) => {
+                                return {
+                                    textDecoration: isActive ? "underline" : "none",
+                                };
+                            }}
+                        to="/a_propos">A Propos</NavLink>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
