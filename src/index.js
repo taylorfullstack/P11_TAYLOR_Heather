b0be1ff542a8aaed4bb3891480b92116892d02c7
htaylor91 <htaylor91@gmail.com>
2023-02-17 15:04:03 +0100

    feat: ✨ add React Router to index.js
    use newest version of React Router, 6.8.1 with createBrowserRouter
    
    routing

--- src/index.js
+++ src/index.js
@@ -1,14 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import './styles/index.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import reportWebVitals from './reportWebVitals';
+import {
+    createBrowserRouter,
+    RouterProvider,
+    } from "react-router-dom";
+
+import Root from "./pages/Root.jsx";
+import Home from "./pages/Home.jsx";
+import LogementPage from "./pages/LogementPage.jsx";
+import About from "./pages/About.jsx";
+import ErrorPage from "./pages/ErrorPage.jsx";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Root />,
+        errorElement: <ErrorPage />,
+        children: [
+            {
+                path: "/",
+                element: <Home />
+            },
+            {
+                path: "/logement/:detailsId",
+                element: <LogementPage />,
+            },
+            {
+                path: "/a_propos",
+                element: <About />
+            }
+        ],
+    },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
