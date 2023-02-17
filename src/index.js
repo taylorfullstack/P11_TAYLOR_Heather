949a5c129828337bba68ba4c5a165910c694bf50
htaylor91 <htaylor91@gmail.com>
2023-02-17 19:27:09 +0100

    feat(index.js): 🚚 update react router to treat homepage as index

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ const router = createBrowserRouter([
         errorElement: <ErrorPage />,
         children: [
             {
-                path: "/",
+                index: true,
                 element: <Home />
             },
             {
