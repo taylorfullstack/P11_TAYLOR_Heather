050341d9322d9a4f2cf825158995c3c9afcd1b0a
htaylor91 <htaylor91@gmail.com>
2023-02-22 14:51:07 +0100

    feat(Logement): 🎨 update class names and structure of Logement component

--- src/components/Logement.jsx
+++ src/components/Logement.jsx
@@ -10,22 +10,22 @@ export default function Logement({...logement}) {
     return (
         <article className="logement">
             <Carousel images={logement.pictures} title={logement.title}/>
-            <section className="logement-text-wrapper">
-                <div className="location-wrapper">
-                    <h1>{logement.title}</h1>
-                    <h2>{logement.location}</h2>
+            <section className="article-section">
+                <div className="title-tag-wrapper">
+                    <div className="title-wrapper">
+                        <h1>{logement.title}</h1>
+                        <h2>{logement.location}</h2>
+                    </div>
                     <Tags tags={logement.tags}/>
                 </div>
-
-                <div className="rate-host-wrapper">
+                <div className="host-rate-wrapper">
                     <Host name={logement.host.name} src={logement.host.picture}/>
                     <Rate rate={logement.rating}/>
                 </div>
-                
-                <div className="collapse-wrapper">
-                    <Collapse details={logement.description} summary="Description" page="logement"/>
-                    <Collapse details={logement.equipments} summary="Équipements" page="logement"/>
-                </div>
+            </section>
+            <section className="collapse-wrapper">
+                <Collapse details={logement.description} summary="Description" page="logement"/>
+                <Collapse details={logement.equipments} summary="Équipements" page="logement"/>
             </section>
         </article>
     )
